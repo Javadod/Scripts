eb3b4a0fa6bc8776d624baf7850aeb81007afe5a
Francisco Inostroza Núñez <franciscoinostroza@outlook.com>
2021-06-25 17:24:07 -0400

    Update main.js

--- Backtracking/main.js
+++ Backtracking/main.js
@@ -8,6 +8,7 @@ let contador_interno_rutas = 1
 let insert_sql = `insert into ruta(id, nombre, descripcion, id_estacion_origen, id_estacion_destino) values`
 let ruta_modificada
 let lista_values_sql = []
+let tabla_ruta_estaciones_datos = []
 let size
 
 for (let i = 0; i < todas_las_rutas.length; i++) {
@@ -19,6 +20,7 @@ for (let i = 0; i < todas_las_rutas.length; i++) {
             lista_values_sql.push(
                 `(${contador}, 'ruta ${contador_interno_rutas} ${ruta_modificada.substring(0, 1)}-${ruta_modificada.substring(size-1, size)}', '${ruta_modificada}', ${ruta_modificada.substring(0, 1)}, ${ruta_modificada.substring(size-1, size)});`
             )
+            tabla_ruta_estaciones_datos = tabla_ruta_estaciones_datos.concat(crear_datos_tabla_ruta_estacion(contador, ruta))
             contador_interno_rutas += 1
             contador += 1
         }
@@ -32,7 +34,7 @@ for (let i = 0; i < todas_las_rutas.length; i++) {
 for (let i = 0; i < lista_values_sql.length; i++) {
     const values = lista_values_sql[i]
     let insert = `${insert_sql} ${values}`
-    console.log(insert)
+    // console.log(insert)
 }
 
 // Guarda las estaciones en la tabla estacion
@@ -40,5 +42,19 @@ let estaciones = mostrar_estaciones()
 let insert_sql_tabla_estacion = `insert into estacion(id) values`
 for (const e of estaciones) {
     let insert = `${insert_sql_tabla_estacion} (${e});`
-    console.log(insert)
+    // console.log(insert)
 }
+
+function crear_datos_tabla_ruta_estacion (id, ruta) {
+    let insert_sql_tabla_ruta_estacion = `insert into ruta_estacion(id_ruta, id_estacion) values`
+    // Guarda las estaciones en la tabla ruta_estacion
+    let lista = ruta.split(/,/g)
+    let datos_tabla = []
+    for (const e of lista) {
+        let insert = `${insert_sql_tabla_ruta_estacion} (${id}, ${e});`
+        datos_tabla.push(insert)
+    }
+    return datos_tabla
+}
+
+console.log(tabla_ruta_estaciones_datos)
